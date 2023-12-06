93676239e5880a1df3103c8ad59314c7ddd71b31
YeeFong21 <23004901@siswa.um.edu.my>
2023-12-06 22:00:24 +0800

    First commit

---
+++ HelloWorldApp.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.helloworldapp;
+
+/**
+ *
+ * @author User
+ */
+public class HelloWorldApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+   
